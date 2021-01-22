a73187dd854fe4bde8ae8988c25b4a805ac81708
pragus99 <prayogo0399@gmail.com>
2021-01-22 09:53:22 +0700

    change nvabar

--- resources/views/backend/layouts/nav.blade.php
+++ resources/views/backend/layouts/nav.blade.php
@@ -4,13 +4,14 @@
             <a class="btn btn-navbar" data-toggle="collapse" data-target=".navbar-inverse-collapse">
                 <i class="icon-reorder shaded"></i></a><a class="brand" href="/home">Quiz</a>
             <div class="nav-collapse collapse navbar-inverse-collapse">
-                <li class="nav-item dropdown">
-                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                <ul class="nav pull-right">
+                    <li class="nav-user">
+                        <a class="nav-link" href="#" role="button"
                         data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                         {{ Auth::user()->name }}
                     </a>
-                    </div>
-                </li>
+                    </li>
+                </ul>
             </div>
             <!-- /.nav-collapse -->
         </div>
