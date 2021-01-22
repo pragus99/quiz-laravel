d20c7eaa72d36bdd45120b8f82f2ca24b73cf47a
pragus99 <prayogo0399@gmail.com>
2021-01-22 09:47:12 +0700

    change navbar appreance

--- resources/views/backend/layouts/nav.blade.php
+++ resources/views/backend/layouts/nav.blade.php
@@ -2,43 +2,25 @@
     <div class="navbar-inner">
         <div class="container">
             <a class="btn btn-navbar" data-toggle="collapse" data-target=".navbar-inverse-collapse">
-                <i class="icon-reorder shaded"></i></a><a class="brand" href="/home">Edmin </a>
+                <i class="icon-reorder shaded"></i></a><a class="brand" href="/home">Quiz</a>
             <div class="nav-collapse collapse navbar-inverse-collapse">
-                <ul class="nav nav-icons">
-                    <li class="active"><a href="#"><i class="icon-envelope"></i></a></li>
-                    <li><a href="#"><i class="icon-eye-open"></i></a></li>
-                    <li><a href="#"><i class="icon-bar-chart"></i></a></li>
-                </ul>
-                <form class="navbar-search pull-left input-append" action="#">
-                <input type="text" class="span3">
-                <button class="btn" type="button">
-                    <i class="icon-search"></i>
-                </button>
-                </form>
-                <ul class="nav pull-right">
-                    <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown
-                        <b class="caret"></b></a>
-                        <ul class="dropdown-menu">
-                            <li><a href="#">Item No. 1</a></li>
-                            <li><a href="#">Don't Click</a></li>
-                            <li class="divider"></li>
-                            <li class="nav-header">Example Header</li>
-                            <li><a href="#">A Separated link</a></li>
-                        </ul>
-                    </li>
-                    <li><a href="#">Support </a></li>
-                    <li class="nav-user dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                        <img src="{{ asset('edmin/code/images/user.png') }}" class="nav-avatar" />
-                        <b class="caret"></b></a>
-                        <ul class="dropdown-menu">
-                            <li><a href="#">Your Profile</a></li>
-                            <li><a href="#">Edit Profile</a></li>
-                            <li><a href="#">Account Settings</a></li>
-                            <li class="divider"></li>
-                            <li><a href="#">Logout</a></li>
-                        </ul>
-                    </li>
-                </ul>
+                <li class="nav-item dropdown">
+                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                        {{ Auth::user()->name }}
+                    </a>
+
+                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                             document.getElementById('logout-form').submit();">
+                            {{ __('Logout') }}
+                        </a>
+
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                            @csrf
+                        </form>
+                    </div>
+                </li>
             </div>
             <!-- /.nav-collapse -->
         </div>
